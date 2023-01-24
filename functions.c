47f0605f43c1304222add489d1bda1bccf58875d
LSparko <Akojurudaniel@gmail.com>
2023-01-24 05:12:17 -0800

    update

--- functions.c
+++ functions.c
@@ -46,46 +46,46 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int len = 0, i;
-	char *string = va_arg(types, char *);
+	int length = 0, i;
+	char *str = va_arg(types, char *);
 
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	if (string == NULL)
+	if (str == NULL)
 	{
-		string = "(null)";
+		str = "(null)";
 		if (precision >= 6)
-			string = " ";
+			str = " ";
 	}
 
-	while (string[len] != '\0')
-		len++;
+	while (str[length] != '\0')
+		length++;
 
-	if (precision >= 0 && precision < len)
-		len = precision;
+	if (precision >= 0 && precision < length)
+		length = precision;
 
-	if (width > len)
+	if (width > length)
 	{
 		if (flags & F_MINUS)
 		{
-			write(1, &string[0], len);
-			for (i = width - len; i > 0; i--)
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
 			return (width);
 		}
 		else
 		{
-			for (i = width - len; i > 0; i--)
+			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
-			write(1, &string[0], len);
+			write(1, &str[0], length);
 			return (width);
 		}
 	}
 
-	return (write(1, string, len));
+	return (write(1, str, length));
 }
 
  /**
